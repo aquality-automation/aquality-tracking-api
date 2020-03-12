24320baeae746ff5924436284cadaceca44289ac
VladKostyukevich <vlad.kostyukevich@gmail.com>
2020-03-12 13:53:39 +0300

    fix deps

--- src/test/java/tests/workers/dao/project/Step2TestDaoTest.java
+++ src/test/java/tests/workers/dao/project/Step2TestDaoTest.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mysql.cj.core.conf.url.ConnectionUrlParser.Pair;
 import main.exceptions.AqualityException;
 import main.model.db.dao.project.Step2TestDao;
-import main.model.dto.Step2TestDto;
+import main.model.dto.project.Step2TestDto;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.testng.annotations.BeforeMethod;
--- src/test/java/tests/workers/dao/project/Suite2DashboardDaoTest.java
+++ src/test/java/tests/workers/dao/project/Suite2DashboardDaoTest.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mysql.cj.core.conf.url.ConnectionUrlParser.Pair;
 import main.exceptions.AqualityException;
 import main.model.db.dao.project.Suite2DashboardDao;
-import main.model.dto.Suite2DashboardDto;
+import main.model.dto.project.Suite2DashboardDto;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.testng.annotations.BeforeMethod;
