7069606072e5a697be96f42cb5241038fc0773c8
Andrei Pavar <a.pavar@yandex.ru>
2023-11-10 11:59:11 +0300

    change version in pom

--- src/main/java/main/exceptions/AqualityAuthenticationException.java
+++ src/main/java/main/exceptions/AqualityAuthenticationException.java
@@ -1,6 +1,6 @@
 package main.exceptions;
 
-public class AqualityAuthenticationException extends  AqualityException {
+public class AqualityAuthenticationException extends AqualityException {
     public AqualityAuthenticationException(String error)  {
         super(error);
         this.responseCode = 401;
